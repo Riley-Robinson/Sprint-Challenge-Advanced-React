4870223f015f76b40d9af51c4accf75d82303b29
Riley-Robinson <rrtoinfinity@gmail.com>
2020-04-10 12:18:42 -0400

    use Local Storage done

--- client/src/components/Hooks/useLocalStorage.js
+++ client/src/components/Hooks/useLocalStorage.js
@@ -1,3 +1,24 @@
 import { useState } from "react";
 
-    export const useLocalStorage = (key, value) 
+
+export const useLocalStorage = (key, value) => {
+
+
+            if (window.localStorage && window.localStorage.getItem(key)) {
+                    value = JSON.parse(window.localStorage.getItem(key))
+            }
+
+            else{
+                window.localStorage.setItem(key, JSON.stringify(value))
+            }
+
+
+            cosnt [storedValue, setStoredValue] = useState(value)
+
+            const setLocalStorage = (value) => {
+                window.localStorage.setItem(key, JSON.stringify(value))
+                setStoredValue(value)
+            }
+
+            return[storedValue, setLocalStorage]
+}
